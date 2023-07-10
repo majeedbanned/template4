5128f107d09ec432e8067881d927f9946389e374
majid ghasemi <majidghasemi@majids-MacBook-Pro.local>
2023-07-10 11:25:41 +0330

    prisma adding

--- app/[lang]/(admin)/admin/dashboard/loading.tsx
+++ app/[lang]/(admin)/admin/dashboard/loading.tsx
@@ -3,5 +3,11 @@ import React from "react";
 type Props = {};
 
 export default function loading({}: Props) {
-  return <div>loading</div>;
+  return (
+    <div>
+      loading111 <br />
+      loading111 <br />
+      loading111
+    </div>
+  );
 }
