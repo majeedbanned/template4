dac81bbf7c0233f621a1e41e2a7fbc5857fb1e71
majid ghasemi <majidghasemi@majids-MacBook-Pro.local>
2023-07-10 23:31:11 +0330

    auth implimention

---
+++ app/[lang]/(auth)/signin/page.tsx
@@ -0,0 +1,13 @@
+import Login from "@/components/auth/login";
+import React from "react";
+
+type Props = {};
+
+export default function page({}: Props) {
+  return (
+    <div>
+      LOgin
+      <Login></Login>
+    </div>
+  );
+}
---
+++ app/[lang]/(auth)/signup/page.tsx
@@ -0,0 +1,8 @@
+import Register from "@/components/auth/register";
+import React from "react";
+
+type Props = {};
+
+export default function page({}: Props) {
+  return <Register></Register>;
+}
