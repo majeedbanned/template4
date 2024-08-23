98ce030bb6736db6067eed7f3210cfe2dbd5af74
majid <majeed.ghasemi@gmail.com>
2024-08-23 22:09:16 +0330

    c9

--- app/[lang]/callback/page.tsx
+++ app/[lang]/callback/page.tsx
@@ -39,7 +39,7 @@ export default async function page({}: Props) {
         </div>
       </div>
 
-      <PostedDataDisplay></PostedDataDisplay>
+      {/* <PostedDataDisplay></PostedDataDisplay> */}
 
       {/* <Form></Form> */}
     </div>
