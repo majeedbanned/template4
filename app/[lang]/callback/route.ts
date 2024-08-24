d0ca4c7a0be4064b913a9b83e5abd540df60ab27
majid <majeed.ghasemi@gmail.com>
2024-08-24 07:55:10 +0330

    c20

--- app/[lang]/callback/route.ts
+++ app/[lang]/callback/route.ts
@@ -3,26 +3,40 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function POST(req: NextRequest) {
   console.log('Received POST request');
 
-  // Check if the Content-Type is application/json
   const contentType = req.headers.get('content-type') || '';
   if (!contentType.includes('application/json')) {
     return NextResponse.json({ error: 'Invalid content type, expected application/json' }, { status: 400 });
   }
 
   try {
-    // Read the raw body (for debugging purposes)
     const rawBody = await req.text();
     console.log('Raw body:', rawBody);
 
-    // Parse the incoming request's JSON body
     const data = JSON.parse(rawBody);
     console.log('Parsed data:', data);
 
-    // Return the parsed data as JSON in the response
     return NextResponse.json({ receivedData: data });
   } catch (error) {
     console.error('Error parsing JSON:', error);
-    // Handle any errors that occur during parsing or processing
     return NextResponse.json({ error: 'Failed to parse JSON' }, { status: 400 });
   }
+}
+
+export async function GET(req: NextRequest) {
+  console.log('Received GET request');
+
+  // Get the query parameters from the request URL
+  const { searchParams } = new URL(req.url);
+  const queryParams = {};
+
+  // Convert the query parameters into a JavaScript object
+  searchParams.forEach((value, key) => {
+  //@ts-ignore
+    queryParams[key] = value;
+  });
+
+  console.log('Query parameters:', queryParams);
+
+  // Return the query parameters as a JSON response
+  return NextResponse.json({ receivedQueryParams: queryParams });
 }
